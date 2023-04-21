c1e2a5ddaa02608e5a0d0c8e221af55c5a712dd9
Ashwini Bhoskar <ashwinibhoskar5111@gmail.com>
2023-04-21 11:04:14 +0530

    added

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,229 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_header_0_header_0_phone.png",
+        "test": "..\\bitmaps_test\\20230421-103526\\backstop_default_header_0_header_0_phone.png",
+        "selector": ".header",
+        "fileName": "backstop_default_header_0_header_0_phone.png",
+        "label": "header",
+        "misMatchThreshold": 0.1,
+        "url": "http://127.0.0.1:5500/index.html",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_header_0_header_1_deskstop.png",
+        "test": "..\\bitmaps_test\\20230421-103526\\backstop_default_header_0_header_1_deskstop.png",
+        "selector": ".header",
+        "fileName": "backstop_default_header_0_header_1_deskstop.png",
+        "label": "header",
+        "misMatchThreshold": 0.1,
+        "url": "http://127.0.0.1:5500/index.html",
+        "expect": 0,
+        "viewportLabel": "deskstop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_showcase_0_showcase_0_phone.png",
+        "test": "..\\bitmaps_test\\20230421-103526\\backstop_default_showcase_0_showcase_0_phone.png",
+        "selector": ".showcase",
+        "fileName": "backstop_default_showcase_0_showcase_0_phone.png",
+        "label": "showcase",
+        "misMatchThreshold": 0.1,
+        "url": "http://127.0.0.1:5500/index.html",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_showcase_0_showcase_1_deskstop.png",
+        "test": "..\\bitmaps_test\\20230421-103526\\backstop_default_showcase_0_showcase_1_deskstop.png",
+        "selector": ".showcase",
+        "fileName": "backstop_default_showcase_0_showcase_1_deskstop.png",
+        "label": "showcase",
+        "misMatchThreshold": 0.1,
+        "url": "http://127.0.0.1:5500/index.html",
+        "expect": 0,
+        "viewportLabel": "deskstop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_newsletter_0_newsletter_0_phone.png",
+        "test": "..\\bitmaps_test\\20230421-103526\\backstop_default_newsletter_0_newsletter_0_phone.png",
+        "selector": ".newsletter",
+        "fileName": "backstop_default_newsletter_0_newsletter_0_phone.png",
+        "label": "newsletter",
+        "misMatchThreshold": 0.1,
+        "url": "http://127.0.0.1:5500/index.html",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_newsletter_0_newsletter_1_deskstop.png",
+        "test": "..\\bitmaps_test\\20230421-103526\\backstop_default_newsletter_0_newsletter_1_deskstop.png",
+        "selector": ".newsletter",
+        "fileName": "backstop_default_newsletter_0_newsletter_1_deskstop.png",
+        "label": "newsletter",
+        "misMatchThreshold": 0.1,
+        "url": "http://127.0.0.1:5500/index.html",
+        "expect": 0,
+        "viewportLabel": "deskstop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_boxes_0_boxes_0_phone.png",
+        "test": "..\\bitmaps_test\\20230421-103526\\backstop_default_boxes_0_boxes_0_phone.png",
+        "selector": ".boxes",
+        "fileName": "backstop_default_boxes_0_boxes_0_phone.png",
+        "label": "boxes",
+        "misMatchThreshold": 0.1,
+        "url": "http://127.0.0.1:5500/index.html",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_boxes_0_boxes_1_deskstop.png",
+        "test": "..\\bitmaps_test\\20230421-103526\\backstop_default_boxes_0_boxes_1_deskstop.png",
+        "selector": ".boxes",
+        "fileName": "backstop_default_boxes_0_boxes_1_deskstop.png",
+        "label": "boxes",
+        "misMatchThreshold": 0.1,
+        "url": "http://127.0.0.1:5500/index.html",
+        "expect": 0,
+        "viewportLabel": "deskstop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_footer_0_footer_0_phone.png",
+        "test": "..\\bitmaps_test\\20230421-103526\\backstop_default_footer_0_footer_0_phone.png",
+        "selector": ".footer",
+        "fileName": "backstop_default_footer_0_footer_0_phone.png",
+        "label": "footer",
+        "misMatchThreshold": 0.1,
+        "url": "http://127.0.0.1:5500/index.html",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 88,
+            "height": -108
+          },
+          "rawMisMatchPercentage": 39.99652777777778,
+          "misMatchPercentage": "40.00",
+          "analysisTime": 23
+        },
+        "diffImage": "..\\bitmaps_test\\20230421-103526\\failed_diff_backstop_default_footer_0_footer_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_footer_0_footer_1_deskstop.png",
+        "test": "..\\bitmaps_test\\20230421-103526\\backstop_default_footer_0_footer_1_deskstop.png",
+        "selector": ".footer",
+        "fileName": "backstop_default_footer_0_footer_1_deskstop.png",
+        "label": "footer",
+        "misMatchThreshold": 0.1,
+        "url": "http://127.0.0.1:5500/index.html",
+        "expect": 0,
+        "viewportLabel": "deskstop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
